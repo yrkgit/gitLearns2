003b5fcb0df2dd327de12e54b0bcb4b6d9979225
PC <yarkovok@wp.pl>
2021-12-08 19:39:52 +0100

    First commit of project small changes

--- app/src/main/java/com/example/gitlearn2/MainActivity.java
+++ app/src/main/java/com/example/gitlearn2/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+String firstValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("first commit changes");
     }
 }
